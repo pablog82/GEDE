8394a52bcafdf669dc2a2bcf4177107639e96e3c
Álvaro Peláez Piñera <jagargar@ayuntamiento.svq>
2023-11-28 10:23:10 +0100

    Log to file

--- src/main/java/com/babelgroup/gede/service/impl/GestorDocumentalServiceImpl.java
+++ src/main/java/com/babelgroup/gede/service/impl/GestorDocumentalServiceImpl.java
@@ -8,11 +8,9 @@ import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -20,8 +18,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import com.babelgroup.gede.dto.DocumentoResponse;
@@ -129,11 +125,6 @@ public class GestorDocumentalServiceImpl implements GestorDocumentalService {
 							// obtenerTicket();
 
 //							getTiposDocumentales();
-
-							if (!validarTicket()) {
-								obtenerTicket();
-								validarTicket();
-							}
 
 							if (registros.isEmpty()) {
 
