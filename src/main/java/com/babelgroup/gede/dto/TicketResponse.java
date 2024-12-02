dbbb05d3312dd8302e4bafd032f64d7e0d18f36e
Álvaro Peláez Piñera <alvaro.pelaez@babelgroup.com>
2024-12-02 10:10:06 +0100

    Lombok

--- src/main/java/com/babelgroup/gede/dto/TicketResponse.java
+++ src/main/java/com/babelgroup/gede/dto/TicketResponse.java
@@ -10,7 +10,6 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @ToString
 public class TicketResponse {
 	private Boolean exito;
@@ -24,4 +23,17 @@ public class TicketResponse {
 	private Boolean valido;
 	private String identificadorUsuario;
 	private String nombreCompleto;
+
+	public TicketResponse(Boolean exito, List<String> errores, Integer codigoRespuesta, String ticket, String usuario, String organismo, Date caducidad, Boolean valido, String identificadorUsuario, String nombreCompleto) {
+		this.exito = exito;
+		this.errores = errores;
+		this.codigoRespuesta = codigoRespuesta;
+		this.ticket = ticket;
+		this.usuario = usuario;
+		this.organismo = organismo;
+		this.caducidad = caducidad;
+		this.valido = valido;
+		this.identificadorUsuario = identificadorUsuario;
+		this.nombreCompleto = nombreCompleto;
+	}
 }
