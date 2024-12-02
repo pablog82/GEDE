875bac87a5d82fb73f3c893f700f3f8d86b9b7ca
Álvaro Peláez Piñera <alvaro.pelaez@babelgroup.com>
2024-12-02 09:12:12 +0100

    Disable ssl

--- src/main/java/com/babelgroup/gede/GedeApplication.java
+++ src/main/java/com/babelgroup/gede/GedeApplication.java
@@ -1,6 +1,5 @@
 package com.babelgroup.gede;
 
-import java.rmi.registry.Registry;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -8,27 +7,23 @@ import java.security.NoSuchAlgorithmException;
 import javax.net.ssl.SSLContext;
 import javax.sql.DataSource;
 
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
-import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
-import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.core5.http.config.RegistryBuilder;
-import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Security.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.web.client.RestTemplate;
 
 import com.babelgroup.gede.service.GestorDocumentalService;
 
@@ -36,60 +31,86 @@ import com.babelgroup.gede.service.GestorDocumentalService;
  * The Class GedeApplication.
  */
 @SpringBootApplication
-@PropertySources({ @PropertySource("classpath:application.properties"), @PropertySource("classpath:log4j2.properties"),
-		@PropertySource("classpath:database.properties"),
-		@PropertySource("file:${spring.config.location}/afirma.properties"),
-		@PropertySource("file:${spring.config.location}/gede.properties")
+@PropertySources({@PropertySource("classpath:application.properties"), @PropertySource("classpath:log4j2.properties"),
+        @PropertySource("classpath:database.properties"),
+        @PropertySource("file:${spring.config.location}/afirma.properties"),
+        @PropertySource("file:${spring.config.location}/gede.properties")
 //    @PropertySource("classpath:afirma.properties"),
 //    @PropertySource("classpath:gede.properties")
 })
 public class GedeApplication implements CommandLineRunner {
 
-	@Autowired
-	Environment env;
+    @Autowired
+    Environment env;
 
-	@Autowired
-	private GestorDocumentalService gestorDocumentalService;
+    @Autowired
+    private GestorDocumentalService gestorDocumentalService;
 
-	/**
-	 * The main method.
-	 *
-	 * @param args the arguments
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(GedeApplication.class, args).close();
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(GedeApplication.class, args).close();
 
-	}
+    }
 
-	/**
-	 * Run.
-	 *
-	 * @param args the args
-	 * @throws Exception the exception
-	 */
-	@Override
-	public void run(String... args) throws Exception {
-		gestorDocumentalService.subirDocumentos();
-	}
+    /**
+     * Run.
+     *
+     * @param args the args
+     * @throws Exception the exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        gestorDocumentalService.subirDocumentos();
+    }
 
-	/**
-	 * Data source.
-	 *
-	 * @return the data source
-	 */
-	@Bean
-	DataSource dataSource() {
-		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(env.getProperty("sqlite.driver"));
-		dataSource.setUrl(env.getProperty("sqlite.url"));
-		dataSource.setUsername(env.getProperty("sqlite.user"));
-		dataSource.setPassword(env.getProperty("sqlite.password"));
-		return dataSource;
-	}
+    /**
+     * Data source.
+     *
+     * @return the data source
+     */
+    @Bean
+    DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("sqlite.driver"));
+        dataSource.setUrl(env.getProperty("sqlite.url"));
+        dataSource.setUsername(env.getProperty("sqlite.user"));
+        dataSource.setPassword(env.getProperty("sqlite.password"));
+        return dataSource;
+    }
 
-//	@Bean
-//	RestTemplate restTemplate(RestTemplateBuilder builder) {
-//		return builder.build();
-//	}
-	
+    @Bean
+    public RestTemplate getRestTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        TrustStrategy acceptingTrustStrategy = (x509Certificates, s) -> true;
+        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        return restTemplate;
+    }
+//    public RestTemplate createExternal(boolean sslDisabled) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+//        RestTemplate restTemplate = new RestTemplate();
+//        HttpComponentsClientHttpRequestFactory requestFactory = getHttpComponentsClientHttpRequestFactory();
+//        restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(requestFactory));
+//        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory();
+//        uriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+//        restTemplate.setUriTemplateHandler(uriBuilderFactory);
+//        return restTemplate;
+//    }
+//
+//    private static HttpComponentsClientHttpRequestFactory getHttpComponentsClientHttpRequestFactory() throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException {
+//        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial((KeyStore) null, (chain, authType)->true).build();
+//        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+//        HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create().setSSLSocketFactory(csf).build();
+//        CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(connectionManager).build();
+//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+//        requestFactory.setHttpClient(httpClient);
+//        return requestFactory;
+//    }
+
 }
